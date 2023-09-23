5b6fefa67dfeb79b95e32e5cc5f8057efa274faf
RMChannel <98057423+RMChannel@users.noreply.github.com>
2023-09-23 12:54:22 +0200

    V1.0
    Per ora sono disponibili 2 funzioni, da ottale a binario e viceversa

---
+++ functions.py
@@ -0,0 +1,73 @@
+import os
+numbers="1234567890"
+octalbin={
+    "0":"000",
+    "1":"001",
+    "2":"010",
+    "3":"011",
+    "4":"100",
+    "5":"101",
+    "6":"110",
+    "7":"111"
+}
+def insertoctal():
+    octalnum=["8","9"]
+    go=True
+    while go:
+        n=input()
+        if (octalnum[0] in n) or (octalnum[1] in n): print("Numero ottale non corretto, riprovare")
+        else:
+            try:
+                int(n)
+                go=False
+            except ValueError:
+                print("Caratteri non riconosciuti, riprovare")
+                go=True
+    return n
+
+def insertbin():
+    binnum=["2","3","4","5","6","7","8","9"]
+    go=True
+    while go:
+        n=input()
+        for i in binnum:
+            if i in n:
+                print("Numero non corretto, riprovare")
+                go=True
+                break
+            elif (i=="9"): return n
+
+def binseparator(n, bin):
+    result=[]
+    temp=""
+    i=0
+    for c in bin:
+        if i!=n:
+            temp=temp+c
+            i+=1
+        else:
+            result.append(temp)
+            temp=c
+            i=0
+    result.append(temp)
+    return result
+
+
+def octaltobin(num):
+    result=""
+    for i in num: result=result+octalbin[i]
+    return result
+
+def bintooctal(num):
+    result=""
+    while (len(num)%3)!=0:
+        num="0"+num
+    num=binseparator(3,num)
+    for i in num:
+        i2=0
+        while True:
+            if i==octalbin[str(i2)]:
+                result=result+str(i2)
+                break
+            else: i2+=1
+    return result
---
+++ script.py
@@ -0,0 +1,62 @@
+import os
+import functions
+numbers="1234567890"
+listofconvert={
+    1:"Ottale",
+    2:"Binario"
+}
+while True:
+    go=True
+    #Funzione del menu e controllo del dato inserito
+    while go:
+        firstbase=input("Benvenuto in Ultimate Converter V1.0\nScegli la base del numero iniziale:\n[1]Ottale\n[2]Binario\n")
+        if (firstbase in numbers):
+            if not(int(firstbase) in listofconvert):
+                print("Carattere non riconosciuto, rirpovare")
+                os.system("pause")
+                os.system("cls")
+            else:
+                go=False
+                firstbase=int(firstbase)
+        else:
+            print("Carattere non riconosciuto, rirpovare")
+            os.system("pause")
+            os.system("cls")
+
+    #Scelta della base in cui covertire il numero con contro9llo      
+    message="Scegli la base in cui covertire il numero:"
+    i=1
+    while i<3:
+        if i!=firstbase: message=message+"\n["+str(i)+"]"+listofconvert[i]
+        i+=1
+    go=True
+    while go:
+        secondbase=input(message+"\n")
+        if (secondbase in numbers):
+            if (not(int(secondbase) in listofconvert) or (int(secondbase)==firstbase)):
+                print("Carattere non riconosciuto, rirpovare")
+                os.system("pause")
+                os.system("cls")
+            else:
+                go=False
+                secondbase=int(secondbase)
+        else:
+            print("Carattere non riconosciuto, rirpovare")
+            os.system("pause")
+            os.system("cls")
+
+    #Inserimento 1°valore
+    if firstbase==1:
+        print("Inserisci il numero in ottale")
+        num=functions.insertoctal()
+    elif firstbase==2:
+        print("Inserisci il numero in binario")
+        num=functions.insertbin()
+    #Switch della funzione
+    #OTTALE A BINARIO E VICEVERSA
+    if (firstbase==1 and secondbase==2): result=functions.octaltobin(num)
+    elif (firstbase==2 and secondbase==1): result=functions.bintooctal(num)
+    #Stampa del risultato
+    choice=input("Il risultato è: "+result+"\nVuoi ricominciare?\n[1]Sì\n[Altro]No\n")
+    if choice!="1": exit()
+    else: os.system("cls")
