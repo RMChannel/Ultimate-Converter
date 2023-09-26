a15398fb5df1212c71bec2489dd07c838f5b96ab
RMChannel <98057423+RMChannel@users.noreply.github.com>
2023-09-26 10:23:17 +0200

    V1.2
    Aggiunte le conversioni da esadecimale a binario e viceversa e da ottale a decimale e viceversa, inoltre ho coretto un bug nella funzione binseparator

--- functions.py
+++ functions.py
@@ -9,12 +9,31 @@ octalbin={
     "6":"110",
     "7":"111"
 }
+esabin={
+    "0":"0000",
+    "1":"0001",
+    "2":"0010",
+    "3":"0011",
+    "4":"0100",
+    "5":"0101",
+    "6":"0110",
+    "7":"0111",
+    "8":"1000",
+    "9":"1001",
+    "A":"1010",
+    "B":"1011",
+    "C":"1100",
+    "D":"1101",
+    "E":"1110",
+    "F":"1111",
+}
+esachar=["1","2","3","4","5","6","7","8","9","0","A","B","C","D","E","F"]
 def insertoctal():
     octalnum=["8","9"]
     go=True
     while go:
         n=input()
-        if (octalnum[0] in n) or (octalnum[1] in n): print("Numero ottale non corretto, riprovare")
+        if (octalnum[0] in n) or (octalnum[1] in n) or (n==""): print("Numero ottale non corretto, riprovare")
         else:
             try:
                 int(n)
@@ -30,7 +49,7 @@ def insertbin():
     while go:
         n=input()
         for i in binnum:
-            if i in n:
+            if (i in n) or (n==""):
                 print("Numero non corretto, riprovare")
                 go=True
                 break
@@ -48,10 +67,27 @@ def insertnum():
             go=True
     return n
 
+def insertesa():
+    lowchar=["a","b","c","d","e","f"]
+    go=True
+    while go:
+        n=input()
+        for i in n:
+            if i in lowchar: n=n.replace(i,esachar[(lowchar.index(i))+10])
+            if not(i in esachar) or (n==""):
+                print("Numero non corretto, riprovare")
+                go=True
+                break
+            else:
+                go=False
+    return n
+
 def binseparator(n, bin):
     result=[]
     temp=""
     i=0
+    while (len(bin)%n)!=0:
+        bin="0"+bin
     for c in bin:
         if i!=n:
             temp=temp+c
@@ -59,7 +95,7 @@ def binseparator(n, bin):
         else:
             result.append(temp)
             temp=c
-            i=0
+            i=1
     result.append(temp)
     return result
 
@@ -70,10 +106,8 @@ def octaltobin(num):
     return result
 
 def bintooctal(num):
-    result=""
-    while (len(num)%3)!=0:
-        num="0"+num
     num=binseparator(3,num)
+    result=""
     for i in num:
         i2=0
         while True:
@@ -100,3 +134,41 @@ def dectobin(num):
         result=str(num%2)+result
         num=int(num/2)
     return "1"+result
+
+def octaltodec(num):
+    l=len(num)-1
+    result=0
+    for i in num:
+        if l>0:
+            result=result+(pow(8,l)*int(i))
+            l-=1
+        else: result=result+int(i)
+    return str(result)
+
+def dectooctal(num):
+    num=int(num)
+    result=""
+    while num>0:
+        result=str(num%8)+result
+        num=int(num/8)
+    return result
+
+def bintoexa(num):
+    result=""
+    num=binseparator(4,num)
+    print(num)
+    for i in num:
+        i2=0
+        while True:
+            if i2>9: i3=esachar[i2]
+            else: i3=i2
+            if i==esabin[str(i3)]:
+                result=result+str(i3)
+                break
+            else: i2+=1
+    return result
+
+def exatobin(num):
+    result=""
+    for i in num: result=result+esabin[i]
+    return result
--- script.py
+++ script.py
@@ -4,13 +4,14 @@ numbers="1234567890"
 listofconvert={
     1:"Ottale",
     2:"Binario",
-    3:"Decimale"
+    3:"Decimale",
+    4:"Esadecimale"
 }
 while True:
     go=True
     #Funzione del menu e controllo del dato inserito
     while go:
-        firstbase=input("Benvenuto in Ultimate Converter V1.1\nScegli la base del numero iniziale:\n[1]Ottale\n[2]Binario\n[3]Decimale\n")
+        firstbase=input("Benvenuto in Ultimate Converter V1.2\nScegli la base del numero iniziale:\n[1]Ottale\n[2]Binario\n[3]Decimale\n[4]Esadecimale\n")
         if (firstbase in numbers):
             if not(int(firstbase) in listofconvert):
                 print("Carattere non riconosciuto, rirpovare")
@@ -27,7 +28,7 @@ while True:
     #Scelta della base in cui covertire il numero con contro9llo      
     message="Scegli la base in cui covertire il numero:"
     i=1
-    while i<4:
+    while i<5:
         if i!=firstbase: message=message+"\n["+str(i)+"]"+listofconvert[i]
         i+=1
     go=True
@@ -56,6 +57,9 @@ while True:
     elif firstbase==3:
         print("Inserisci un numero decimale")
         num=functions.insertnum()
+    elif firstbase==4:
+        print("Inserisci un numero esadecimale")
+        num=functions.insertesa()
     #Switch della funzione
     #OTTALE A BINARIO E VICEVERSA
     if (firstbase==1 and secondbase==2): result=functions.octaltobin(num)
@@ -63,6 +67,16 @@ while True:
     #DECIMALE A BINARIO E VICEVERSA
     elif (firstbase==3 and secondbase==2): result=functions.dectobin(num)
     elif (firstbase==2 and secondbase==3): result=functions.bintodec(num)
+    #OTTALE A DECIMALE E VICEVERSA
+    elif (firstbase==1 and secondbase==3): result=functions.octaltodec(num)
+    elif (firstbase==3 and secondbase==1): result=functions.dectooctal(num)
+    #ESADECIMALE A BINARIO E VICEVERSA
+    elif (firstbase==2 and secondbase==4): result=functions.bintoexa(num)
+    elif (firstbase==4 and secondbase==2): result=functions.exatobin(num)
+    #OPZIONE DEMO
+    else:
+        print("Mi dispiace, opzione non ancora programmata")
+        exit()
     #Stampa del risultato
     choice=input("Il risultato è: "+result+"\nVuoi ricominciare?\n[1]Sì\n[Altro]No\n")
     if choice!="1": exit()
