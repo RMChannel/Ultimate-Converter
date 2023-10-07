bd173a71d1b25718e687619ffd37c34ff158425d
RMChannel <98057423+RMChannel@users.noreply.github.com>
2023-10-07 10:13:24 +0200

    V1.2
    Ora il programma non sfrutta più la libreria math.h per la potenza, ma sfrutta una funzione creata da me direttamente nel codice, inoltre ora è possibile convertire anche il decimale in ottale e binario e viceversa!!!

--- script.c
+++ script.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <math.h>
 int system(const char *command);
+
+int intpow(int a, int b) {
+    int result=1;
+    if (b!=0) {
+        for (int i=0;i<b;i++) {
+            result=result*a;
+        }
+    }
+    else {
+        a=1;
+    }
+    return result;
+}
 
 void binadd(char *numchar, int n) {
     while ((strlen(numchar)%n)>0) {
@@ -13,6 +25,7 @@ void binadd(char *numchar, int n) {
     }
 }
 
+
 int bintooctal(char *numchar) {
     int result=0, i2=0, i3=0, a=0;
     for (int i=strlen(numchar)-1; i>=0; i--) {
@@ -21,7 +34,7 @@ int bintooctal(char *numchar) {
             i3++;
         }
         if (numchar[i]=='1') {
-            result=(result+(pow(2,i2))*(pow(10,i3)));
+            result=(result+(intpow(2,i2))*(intpow(10,i3)));
         }
         i2++;
     }
@@ -46,47 +59,115 @@ void octaltobin(int numint, char *numchar) {
     }
 }
 
+
+void inttobin(int numint, char *numchar) {
+    while (numint>0) {
+        int temp=numint%2;
+        for (int i=strlen(numchar); i>=0; i--) {
+            numchar[(i+1)]=numchar[i];
+        }
+        if (temp==1) {
+            numchar[0]='1';
+        }
+        else {
+            numchar[0]='0';
+        }
+        numint/=2;
+    }
+}
+
+
+int bintoint(char *numchar) {
+    int result=0;
+    int i2=strlen(numchar)-1;
+    for (int i=0;i<strlen(numchar);i++) {
+        if (numchar[i]=='1') {
+            result=result+(intpow(2,i2));
+        }
+        i2--;
+    }
+    return result;
+}
+
+
+int inttooct(int numint) {
+    int result=0;
+    int i=0;
+    while (numint>0) {
+        int temp=(numint%8)*(intpow(10,i));
+        result=result+temp;
+        numint=numint/8;
+        i++;
+    }
+    return result;
+}
+
+
 int main(void) {
     system("cls");
     char numchar[1000];
-    int firstbase, secondbase;
+    numchar[0]='\0';
+    int firstbase, secondbase, result;
     long int numint;
-    const char listofconvert[3][7] = {"Ottale","Binario"};
-    printf("Benvenuto in Ultimate Converter V1.0\nScegli la base del numero iniziale:\n[1]Ottale\n[2]Binario\n");
+    const char listofconvert[4][10] = {"Ottale","Binario","Decimale"};
+    printf("Benvenuto in Ultimate Converter V1.2\nScegli la base del numero iniziale:\n[1]Ottale\n[2]Binario\n[3]Decimale\n");
     scanf("%d",&firstbase);
     printf("Scegli la base in cui covertire il numero:\n");
-    for (int i=1; i<3; i++) {if (i!=firstbase) {printf("[%d]%s\n", i, listofconvert[(i-1)]);}}
+    for (int i=1; i<4; i++) {if (i!=firstbase) {printf("[%d]%s\n", i, listofconvert[(i-1)]);}}
     scanf("%d",&secondbase);
     switch(firstbase) {
-        case 1: {
+        case 1:
             printf("Insersci il numero in ottale: ");
             scanf("%ld",&numint);
             break;
-        }
-        case 2: {
+        case 2:
             printf("Insersci il numero in binario (massimo 999 cifre): ");
             scanf("%s",&numchar);
             break;
-        }
+        case 3:
+            printf("Inserisci un numero intero: ");
+            scanf("%ld", &numint);
+            break;
     }
     if (firstbase==1) {
         switch(secondbase) {
             case 2:
                 octaltobin(numint,numchar);
-                printf("Result=");
+                printf("Result= ");
                 for (int i=0;i<strlen(numchar);i++) {
                     printf("%c",numchar[i]);
                 }
-            break;
+                break;
+            case 3:
+                break;
         }
     }
     else if (firstbase==2) {
         switch(secondbase) {
             case 1:
                 binadd(numchar,3);
-                int result=bintooctal(numchar);
-                printf("Result=%d", result);
-            break;
+                result=bintooctal(numchar);
+                printf("Result= %d", result);
+                break;
+            case 3:
+                result=bintoint(numchar);
+                printf("Result= %d",result);
+                break;
+        }
+    }
+    else if (firstbase==3) {
+        switch(secondbase) {
+            case 1:
+                result=inttooct(numint);
+                printf("Result= %d",result);
+                break;
+            case 2:
+                inttobin(numint,numchar);
+                printf("Result= ");
+                for (int i=0;i<strlen(numchar);i++) {
+                    printf("%c",numchar[i]);
+                }
+                break;
         }
     }
 }
